b0d2a3e25f243786b8d1920965fd1e07cb8377db
bkfrye <brandonkfrye@gmail.com>
2022-05-08 18:58:57 -0400

    basic setup to start building out components

--- index.php
+++ index.php
@@ -1,21 +1,22 @@
 <?php
 /**
- * Plugin Name:       create-block
+ * Plugin Name:       Multi Custom Blocks
  * Requires at least: 5.8
  * Requires PHP:      7.0
  * Version:           0.1.0
- * Author:            The WordPress Contributors
+ * Author:            @bkfrye
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       multi-custom-blocks
  *
- * @package           create-block
+ * @package           multi-custom-blocks
  */
 
 function create_block_multi_custom_blocks_block_init() {
 
 	$blocks = array(
-		'multi-custom-blocks/'
+		'image-card/',
+		'image-card-section/'
 	);
 
 	foreach ( $blocks as $block ) {
