80801ec3dd8816221432d5b2a6024b6e124ff986
bkfrye <brandonkfrye@gmail.com>
2022-05-09 12:13:28 -0400

    replace placeholder images with ncsu related photo

--- includes/block-editor/blocks/image-card-section/edit.js
+++ includes/block-editor/blocks/image-card-section/edit.js
@@ -15,7 +15,7 @@ const BLOCKS_TEMPLATE = [
 			[ 'multi-custom-blocks/image-card', { 
 					"title": "Card title 1 goes here",
 					"mediaID": 1,
-					"mediaURL": "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/2ChocolateChipCookies.jpg/320px-2ChocolateChipCookies.jpg",
+					"mediaURL": "./images/background.jpg",
 					"description": "description text goes here",
 					"btn": "<a href='www.google.com' target='_blank'>Button copy</a>"
 				} 
